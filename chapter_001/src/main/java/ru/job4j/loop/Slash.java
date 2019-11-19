b649dea7e1e5c7104f6ce68d8c03dfb20acf5771
VasiaOrlov <ov_prog@mail.ru>
2019-11-19 20:18:10 +0300

    5.9. Крест в псевдографике. [#189813]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,39 @@
+package ru.job4j.loop;
+
+/**
+ * Class Slash.
+ * @author Vasiliy Orlov
+ */
+public class Slash {
+    /**
+     * Method draw.
+     * @param size - size table.
+     */
+    public static void draw(int size) {
+        for(int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                boolean left = row == cell;
+                boolean right = size - row - 1 == cell;
+                if (left) {
+                    System.out.print("0");
+                } else if (right) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Method main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
+    }
+}
