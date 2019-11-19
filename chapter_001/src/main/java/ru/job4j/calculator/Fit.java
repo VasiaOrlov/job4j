4b0197e0bb79da35cff26ca26d9524acf3c5f71b
VasiaOrlov <ov_prog@mail.ru>
2019-11-19 19:34:32 +0300

    5.8. JavaDoc. Code convention[#189807]

--- chapter_001/src/main/java/ru/job4j/calculator/Fit.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Fit.java
@@ -1,12 +1,35 @@
 package ru.job4j.calculator;
 
+/**
+ * Class Fit.
+ * @author Vasiliy Orlov (ov_prog@mail.ru).
+ */
+
 public class Fit {
+
+    /**
+     * Method manWeight.
+     * @param height - height man.
+     * @return  - weight man.
+     */
     public static double manWeight(double height) {
         return (height - 100) * 1.15;
     }
+
+    /**
+     * Method womanWeight.
+     * @param height - height woman.
+     * @return - weight woman.
+     */
+
     public static double womanWeight(double height) {
         return (height - 110) * 1.15;
     }
+
+    /**
+     * Method main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         double man = manWeight(180);
         System.out.println("Man 180 is " + man);
