88ccade86a666ef282e78c7456a7a1b920c75dbd
VasiaOrlov <ov_prog@mail.ru>
2019-11-19 19:18:07 +0300

    5.7. Ипотека.[#189811]

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -5,7 +5,7 @@ public class Mortgage {
         int year = 0;
         while (amount > 0) {
             year++;
-            amount = amount * (100 + (int)(percent))/100 - salary;
+            amount = (int)(amount * (100 + percent)/100 - salary);
         }
         return year;
     }
