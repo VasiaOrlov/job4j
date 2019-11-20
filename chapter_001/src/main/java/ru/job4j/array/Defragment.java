dea3102d75f7286f2b91893c03cb0cf2c296b89c
VasiaOrlov <ov_prog@mail.ru>
2019-11-20 23:41:52 +0300

    6.8. Дефрагментация массива.[#189822] (Ver 2)

--- chapter_001/src/main/java/ru/job4j/array/Defragment.java
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -1,6 +1,15 @@
 package ru.job4j.array;
 
+/**
+ * Class Defragment.
+ * @author Vasiliy Orlov.
+ */
 public class Defragment {
+    /**
+     * Method compress.
+     * @param array - input array.
+     * @return - finish array.
+     */
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             String cell = array[index];
@@ -18,12 +27,48 @@ public class Defragment {
         return array;
     }
 
+    /**
+     * Method compress1.
+     * @param array - input array.
+     * @return - finish array.
+     */
+    public static String[] compress1(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            String cell = array[index];
+            if (cell == null) {
+                int i = index;
+                while (i < array.length - 1) {
+                    if (array[i + 1] != null) {
+                       array[index] = array[i + 1];
+                       array[i + 1] = null;
+                       break;
+                    } else {
+                        i++;
+                    }
+                }
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    /**
+     * Method main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] input1 = {"I", null, "wanna", null, "be", null, "compressed"};
         String[] compressed = compress(input);
+        System.out.println();
+        String[] compressed1 = compress1(input1);
         System.out.println();
         for (int index = 0; index < compressed.length; index++) {
             System.out.print(compressed[index] + " ");
+        }
+        System.out.println();
+        for (int index = 0; index < compressed1.length; index++) {
+            System.out.print(compressed1[index] + " ");
         }
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/DefragmentTest.java
+++ chapter_001/src/test/java/ru/job4j/array/DefragmentTest.java
@@ -4,6 +4,10 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Class DefragmentTest.
+ * @author Vasiliy Orlov.
+ */
 public class DefragmentTest {
     @Test
     public void notFirstNull() {
@@ -17,6 +21,22 @@ public class DefragmentTest {
     public void firstNull() {
         String[] input = {null, "I", "wanna", null, "be", null, "compressed"};
         String[] compressed = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        assertThat(compressed, is(expected));
+    }
+
+    @Test
+    public void notFirstNull1() {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = Defragment.compress1(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        assertThat(compressed, is(expected));
+    }
+
+    @Test
+    public void firstNull1() {
+        String[] input = {null, "I", "wanna", null, "be", null, "compressed"};
+        String[] compressed = Defragment.compress1(input);
         String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
         assertThat(compressed, is(expected));
     }
