f5f50632d6c88dfb08894d7b0dc8363af73e76b7
VasiaOrlov <ov_prog@mail.ru>
2019-11-26 01:04:52 +0300

    1.7. Состояние объекта.[#189950]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+    public void exchanger(Battery another) {
+        another.load += this.load;
+    }
+    public static void main(String[] args) {
+        Battery one = new Battery();
+        one.load = 15;
+        Battery two = new Battery();
+        two.load = 20;
+        one.exchanger(two);
+        System.out.println("charge bat two = " + two.load);
+    }
+}
