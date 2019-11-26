b7a1d797bc5492db1183346879f2372ebce36811
VasiaOrlov <ov_prog@mail.ru>
2019-11-26 21:33:29 +0300

    3. Перегрузить метод max для трех чисел[#189942]

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -22,4 +22,40 @@ public class MaxTest {
         int result2 = Max.max(1, 1);
         assertThat(result2, is(1));
     }
+
+    @Test
+    public void maxTest1() {
+        int rsl = Max.max(2, 4, 5);
+        assertThat(rsl, is(5));
+    }
+
+    @Test
+    public void maxTest2() {
+        int rsl = Max.max(10, 4, 5);
+        assertThat(rsl, is(10));
+    }
+
+    @Test
+    public void maxTest3() {
+        int rsl = Max.max(5, 4, 5);
+        assertThat(rsl, is(5));
+    }
+
+    @Test
+    public void maxTest4() {
+        int rsl = Max.max(2, 4, 5, 10);
+        assertThat(rsl, is(10));
+    }
+
+    @Test
+    public void maxTest5() {
+        int rsl = Max.max(10, 4, 5, 1);
+        assertThat(rsl, is(10));
+    }
+
+    @Test
+    public void maxTest6() {
+        int rsl = Max.max(5, 4, 5, 2);
+        assertThat(rsl, is(5));
+    }
 }
