8f566e80d72c73f63b4c5df7a266ad806560ff55
VasiaOrlov <ov_prog@mail.ru>
2019-12-03 19:38:05 +0300

    2. Scanner и чтение числа из консоли.[#189920] ver2

--- chapter_002/src/main/java/ru/job4j/io/Matches.java
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 /**
  * Class Mathes - игра в убери все спички.
+ *
  * @author Vasiliy Orlov.
  */
 public class Matches {
@@ -14,38 +15,21 @@ public class Matches {
         int match;
         int i = 1;
         while (value > 0) {
-            if (i % 2 != 0) {
-                System.out.println("Ход первого игрока. Сколько спичек убираем?");
-                match = Integer.valueOf(input.nextLine());
-                if (match < 1 || match > 3) {
-                    System.out.println("Введите число от 1 до 3.");
-                    for (; match < 4 && match > 0;) {
-                        match = Integer.valueOf(input.nextLine());
-                    }
-                } else {
-                    value -= match;
-                    i++;
-                    System.out.println("На столе лежит спичек - " + value);
+            String player = i % 2 != 0 ? "первого" : "второго";
+            System.out.println(String.format("Ход %s игрока. Сколько спичек убираем?", player));
+            match = Integer.valueOf(input.nextLine());
+            if (match < 1 || match > 3) {
+                System.out.println("Введите число от 1 до 3.");
+                for (; match < 4 && match > 0;) {
+                    match = Integer.valueOf(input.nextLine());
                 }
             } else {
-                System.out.println("Ход второго игрока. Сколько спичек убираем?");
-                match = Integer.valueOf(input.nextLine());
-                if (match < 1 || match > 3) {
-                    System.out.println("Введите число от 1 до 3.");
-                    for (; match < 4 && match > 0;) {
-                        match = Integer.valueOf(input.nextLine());
-                    }
-                } else {
-                    value -= match;
-                    i++;
-                    System.out.println("На столе лежит спечек - " + value);
-                }
+                value -= match;
+                i++;
+                System.out.println("На столе лежит спичек - " + value);
             }
         }
-        if (i % 2 == 0) {
-            System.out.println("Победил первый игрок");
-        } else {
-            System.out.println("Победил второй игрок");
-        }
+        String player = i % 2 == 0 ? "первый" : "второй";
+        System.out.println(String.format("Победил %s игрок", player));
     }
 }
