504a984afe9a2d4965b5f8f6be74a395f85ebd69
VasiliiOrlov <ov_prog@mail.ru>
2021-03-17 22:06:40 +0300

    Checkstyle fix

--- chapter_002/src/test/java/ru/job4j/tream/StudentLevelTest.java
+++ chapter_002/src/test/java/ru/job4j/tream/StudentLevelTest.java
@@ -14,11 +14,11 @@ public class StudentLevelTest {
     public void whenSorted() {
         List<Student> input = new ArrayList<>();
         input.add(new Student(28, "Masha"));
-        input.add(new Student(128,"Pety"));
+        input.add(new Student(128, "Pety"));
         input.add(new Student(5, "Kolya"));
         List<Student> expected = List.of(
-                new Student(128,"Pety"),
-                new Student(28,"Masha")
+                new Student(128, "Pety"),
+                new Student(28, "Masha")
         );
         assertThat(StudentLevel.levelOf(input, 20), is(expected));
     }
@@ -26,7 +26,7 @@ public class StudentLevelTest {
     @Test
     public void whenOnlyNull() {
         List<Student> input = new ArrayList<>();
-        input.add( null);
+        input.add(null);
         List<Student> expected = List.of();
         assertThat(StudentLevel.levelOf(input, 100), is(expected));
     }
@@ -34,9 +34,9 @@ public class StudentLevelTest {
     @Test
     public void whenHasNull() {
         List<Student> input = new ArrayList<>();
-        input.add( null);
-        input.add(new Student(28,"Pety"));
-        List<Student> expected = List.of(new Student(28,"Pety"));
+        input.add(null);
+        input.add(new Student(28, "Pety"));
+        List<Student> expected = List.of(new Student(28, "Pety"));
         assertThat(StudentLevel.levelOf(input, 10), is(expected));
     }
 }
